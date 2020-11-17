4b6819b938148f24f0dbb3e208b9c3639e2c16c2
冯国清 <master@rebeta.cn>
2020-11-17 16:25:18 +0800

    fix: 订阅消息的考试类型;

--- miniprogram/pages/position-2020-tgjs/index.js
+++ miniprogram/pages/position-2020-tgjs/index.js
@@ -60,7 +60,7 @@ Page({
         confirmText: "免费订阅",
         success(res) {
           if (res.confirm) {
-            getApp().methods.subscribeSingleExam(_this.data.suffix, _this.data.phone, "吉林公务员", undefined, () => {
+            getApp().methods.subscribeSingleExam(_this.data.suffix, _this.data.phone, "特岗教师", undefined, () => {
               _this.seach_result() // 订阅成功后执行查询
             })
           } else if (res.cancel) {
