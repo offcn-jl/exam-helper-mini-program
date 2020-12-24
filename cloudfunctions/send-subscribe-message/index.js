a45338e2ae0d68c2c51662b1ded5b30adf53d15d
冯国清 <master@rebeta.cn>
2020-12-24 10:29:17 +0800

    feat: 优化;
    增加按照订阅的消息模板发送对应的消息的功能; 增加彩色日志打印工具; 优化日志打印逻辑;

--- cloudfunctions/send-subscribe-message/index.js
+++ cloudfunctions/send-subscribe-message/index.js
@@ -3,18 +3,22 @@ const 云开发 = require('wx-server-sdk')
 
 // 测试 test-unokj
 // 生产 release-yum30
-云开发.init({env: "test-unokj"})
+云开发.init({ env: "test-unokj" })
 
 // 云函数入口函数
 exports.main = async () => {
+  console.log("开始执行")
+
   const 数据库 = 云开发.database()
-  const 出公告的考试项目 = "吉林公务员"
-  const 小程序页面路径 = "/pages/ad-navigate-19/index?scene=2020111301"
-  const 考试名称 = "考试名称"
-  const 招聘人数 = "xxxx人"
-  const 报名时间 = "2021/10/1 15:01 ~ 2021/11/1 15:01"
-  const 考试时间 = "2021/10/1 15:01 ~ 2021/11/1 15:01"
-  const 备注 = "备注"
+
+  const 出公告的考试项目 = "银行考试" // 公共配置
+  const 小程序页面路径 = "/pages/ad-navigate-19/index?scene=2020111301" // 公共配置
+  const 考试名称 = "考试名称" // 公共配置
+  const 报名时间 = "2021/10/1 15:01 ~ 2021/11/1 15:01" // 公共配置
+  const 备注 = "备注" // 公共配置
+
+  const 招聘人数 = "xxxx人" // 仅一次性模板可用
+  const 考试时间 = "2021/10/1 15:01 ~ 2021/11/1 15:01" // 仅一次性模板可用
 
   // 获取最大数量
   let 预约记录总数 = await 数据库.collection("subscribeExam").count()
@@ -27,38 +31,80 @@ exports.main = async () => {
     let 查询结果 = await 数据库.collection("subscribeExam").skip(跳过的记录条数).limit(查询结果集数量上限).get()
     if (查询结果.errMsg !== "collection.get:ok") { return "查询出错" }
     查询结果.data.forEach(async 订阅记录 => {
-      console.group("订阅记录 " + 订阅记录._id)
-      console.log("记录: ", 订阅记录)
       let 已发送 = false;
       for (let 计数器 = 0; 计数器 < 订阅记录.subscribe.length; 计数器++) {
         if (订阅记录.subscribe[计数器] === 出公告的考试项目) {
           // 发送
           try {
             if (订阅记录.suffix) { 小程序页面路径 += "*" + 订阅记录.suffix }
-            const result = await 云开发.openapi.subscribeMessage.send({
-              templateId: "r6jJofVAClt9WWoh9XL42bf5z2t7FVUX6DNZvmKuSNY",
-              touser: 订阅记录._openid,
-              page: 小程序页面路径,
-              data: { thing1: { value: 考试名称 }, thing2: { value: 招聘人数 }, time3: { value: 报名时间 }, time4: { value: 考试时间 }, thing5: { value: 备注 } }
-            })
-            console.group("%c发送成功", "background: #00AA13; padding: 2px 4px 2px 4px; border-radius: 3px; color: #fff;")
-            console.log("回执: ", result)
-            console.groupEnd()
-            已发送 = true
-            break
+            let result = null;
+            switch (订阅记录.tmplIds[0]) {
+              case "r6jJofVAClt9WWoh9XL42bf5z2t7FVUX6DNZvmKuSNY":
+                // 一次性订阅模板 - 考试公告发布通知
+                result = await 云开发.openapi.subscribeMessage.send({
+                  templateId: "r6jJofVAClt9WWoh9XL42bf5z2t7FVUX6DNZvmKuSNY",
+                  touser: 订阅记录._openid,
+                  page: 小程序页面路径,
+                  data: { thing1: { value: 考试名称 }, thing2: { value: 招聘人数 }, time3: { value: 报名时间 }, time4: { value: 考试时间 }, thing5: { value: 备注 } }
+                });
+                capsule(订阅记录._id, "发送成功", "success");
+                console.log("订阅记录: ", 订阅记录);
+                console.log("回执: ", result)
+                break;
+              case "Ff-Mi9uy4hb9YxYiYgAwOSlGEXgqTkkoIi5sUsOtaao":
+                // 长期订阅模板 - 报名通知
+                result = await 云开发.openapi.subscribeMessage.send({
+                  templateId: "Ff-Mi9uy4hb9YxYiYgAwOSlGEXgqTkkoIi5sUsOtaao",
+                  touser: 订阅记录._openid,
+                  page: 小程序页面路径,
+                  data: { thing2: { value: 考试名称 }, date3: { value: 报名时间 }, thing5: { value: 备注 } }
+                });
+                capsule(订阅记录._id, "发送成功", "success");
+                console.log("订阅记录: ", 订阅记录);
+                console.log("回执: ", result)
+                break;
+              default:
+                // 未知模板
+                capsule(订阅记录._id, "发送失败", "warning");
+                console.log("订阅记录: ", 订阅记录);
+                break;
+            }
+            已发送 = true;
+            break;
           } catch (err) {
-            console.group("%c发送失败", "background: #C00; padding: 2px 4px 2px 4px; border-radius: 3px; color: #fff;")
-            console.log("记录 ID :" + 订阅记录._id)
+            capsule(订阅记录._id, "发送失败", "danger");
+            console.log("订阅记录: ", 订阅记录);
             console.error(err)
-            console.groupEnd()
-            已发送 = true
+            已发送 = true;
           }
         }
       }
       if (!已发送) {
-        console.log("%c未定阅", "background: #D79700; padding: 2px 4px 2px 4px; border-radius: 3px; color: #fff;")
+        capsule(订阅记录._id, "未定阅", "primary");
+        console.log("订阅记录: ", 订阅记录);
       }
-      console.groupEnd()
     })
   }
+}
+
+function typeColor(type = 'default') {
+  let color = ''
+  switch (type) {
+    case 'default': color = '#35495E'; break
+    case 'primary': color = '#3488ff'; break
+    case 'success': color = '#43B883'; break
+    case 'warning': color = '#e6a23c'; break
+    case 'danger': color = '#f56c6c'; break
+    default: ; break
+  }
+  return color
+}
+
+function capsule(title, info, type = 'primary') {
+  console.log(
+    `%c ` + title + ` %c ` + info + ` %c`,
+    'background:#35495E; padding: 1px; border-radius: 3px 0 0 3px; color: #fff;',
+    `background:` + typeColor(type) + `; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff;`,
+    'background:transparent'
+  )
 }
