5f104c2898caa8dc7e2ae812f72607ac81a6f065
冯国清 <master@rebeta.cn>
2021-01-18 11:56:25 +0800

    feat: 人行招聘职位匹配查询;

---
+++ miniprogram/pages/position-2021-jryh-pboc/index.js
@@ -0,0 +1,135 @@
+// pages/position-2020-jr/index
+
+Page({
+  data: {
+    CRMEFSID: "41a1f8d2894053015f99d6b8f071d945", // CRM 活动表单 ID
+    CRMRemark: "活动编码:HD202101130793,活动表单ID:70603", // CRM 注释
+
+    yearList: ["不限","2021","2020"], // 年份
+    provinceList: ["不限","上海市","北京市","重庆市","天津市","河北省","山西省","内蒙区","辽宁省","吉林省","黑龙江省","浙江省","福建省","江苏省","安徽省","山东省","河南省","湖北省","湖南省","江西省","广东省","广西区","海南省","四川省","贵州省","云南省","西藏区","陕西省","甘肃省","青海省","宁夏区","新疆区"], // 省份
+
+    yearValue: '', // 年份
+    provinceValue: '', // 省份
+
+    suffix: "", // 后缀
+    phone: "", // 用户手机号码
+    tipsToSubscribeMessaged: false, // 是否提示过进行消息订阅
+  },
+
+  // 监听筛选条件切换
+  m_select_touch(e) {
+    switch (e.detail.type) {
+      case "year": // 年份
+        this.setData({ yearValue: this.data.yearList[e.detail.index] })
+        break
+      case "province": // 省份
+        this.setData({ provinceValue: this.data.provinceList[e.detail.index] })
+        break
+    }
+  },
+
+  // 注册（录入crm数据）
+  buttonStart: function (e) {
+    getApp().methods.register(e, this.data.suffix, this.data.CRMEFSID, this.data.CRMRemark, phone => {
+      this.setData({ phone })
+      wx.showModal({ title: '提示', content: '注册成功，请您点击“点击查询”按钮进行查询～', showCancel: false, confirmText: "我知道啦" })
+    })
+  },
+
+  // 提示订阅消息推送
+  tipsToSubscribeMessage() {
+    let _this = this
+    if (!_this.data.tipsToSubscribeMessaged) {
+      _this.setData({ tipsToSubscribeMessaged: true })
+      wx.showModal({
+        title: '提示',
+        content: '您是否需要订阅“银行考试”考试公告？订阅成功后您可以在公告发布时免费获得推送提示～',
+        confirmText: "免费订阅",
+        success(res) {
+          if (res.confirm) {
+            getApp().methods.subscribeSingleExam(_this.data.suffix, _this.data.phone, "银行考试", undefined, () => {
+              _this.seach_result() // 订阅成功后执行查询
+            })
+          } else if (res.cancel) {
+            _this.seach_result() // 执行查询
+          }
+        }
+      })
+    } else {
+      _this.seach_result() // 执行查询
+    }
+  },
+
+  // 搜索
+  async seach_result() {
+    let url = "result/index?scene=" + this.data.suffix
+    if (this.data.yearValue !== "") url += "&year=" + this.data.yearValue
+    if (this.data.provinceValue !== "") url += "&province=" + this.data.provinceValue
+    wx.reLaunch({ url })
+  },
+
+  /**
+	 * 生命周期函数--监听页面加载
+	 */
+  onLoad: function (options) {
+    // 获取后缀
+    if (typeof options.scene !== "undefined") {
+      this.setData({
+        suffix: options.scene
+      })
+    }
+    // 判断是否是单页模式 toto 这里要结合登陆使用
+    if (wx.getLaunchOptionsSync().scene !== 1154) {
+      getApp().methods.login(this.data.CRMEFSID, this.data.suffix, this.data.CRMRemark, phone => this.setData({ phone })) // 登陆
+    }
+  },
+
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+  onReady: function () {},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+  onShow: function () {},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+  onHide: function () {},
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+  onUnload: function () {},
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+  onPullDownRefresh: function () {},
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+  onReachBottom: function () {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: '中国人民银行招聘岗位匹配查询系统',
+      imageUrl: 'http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/jryh/share-20210118.jpg'
+    }
+  },
+
+  /**
+   * 用户点击右上角分享 分享到朋友圈
+   */
+  onShareTimeline: function () {
+    return {
+      title: '中国人民银行招聘岗位匹配查询系统'
+    }
+  }
+})
---
+++ miniprogram/pages/position-2021-jryh-pboc/result/index.js
@@ -0,0 +1,109 @@
+const app = getApp()
+
+Page({
+  data: {
+    suffix: "", // 后缀
+    showId: 0, // 当前显示的元素的 ID
+    query: { // 查询条件
+      limits: 10,
+    }, 
+    zcounts: 0,//最大数据量
+    result: [], // 查询结果
+  },
+
+  // 在页面发生滚动时，计算是否需要切换标题栏样式
+  onPageScroll: function (e) {
+    this.selectComponent('#navigation').swtichNavigation(e)
+  },
+
+  //可收放数据样式
+  more: function (event) {
+    this.setData({
+      showId: event.currentTarget.dataset.index
+    })
+  },
+
+  // 上拉刷新
+  onReachBottom() {
+    if (this.data.result.length >= this.data.zcounts) {
+      wx.showToast({ title: '没有更多数据啦', icon: 'none' })
+      return
+    }
+    this.search()
+  },
+
+  //查询
+  search(){
+    let _this = this //作用域 
+    wx.showLoading({ title: '查询中...', mask: true })
+    wx.request({
+      url: "https://zg99.offcn.com/index/chaxun/getfylist?actid=32724",
+      data: this.data.query,
+      success(res) {
+        wx.hideLoading() // 隐藏 loading
+        try {
+          let list = JSON.parse(res.data.substring(1, res.data.length - 1))//去头尾（）,转为json对象
+          if (list.status !== 1) {//如果status不等于1，弹出错误提示
+            wx.showToast({ title: list.msg, icon: 'none' })
+            return
+          }
+          if (list.lists.length <= 0) {//如果内容长度小于等于0，弹出无数据提示
+            wx.showToast({ title: '没有更多数据啦', icon: 'none' })
+            return
+          }
+          //数据导入data.result
+          _this.setData({
+            result: _this.data.result.concat(list.lists),//不替换原数据添加新数据
+            "query.page": _this.data.result.concat(list.lists).length / _this.data.query.limits,//计算分页页数
+            zcounts: list.zcounts   //总数量录入data
+          })
+        } catch (err) {//捕获错误并报错
+          getApp().methods.handleError({ err, title: "出错啦", content: '查询失败', reLaunch: true })
+        }
+      },
+      fail: err => {//获取失败后提示
+        wx.hideLoading() // 隐藏 loading
+        getApp().methods.handleError({ err: err, title: "出错啦", content: '查询失败', reLaunch: true })
+      }
+    })
+  },
+
+  onLoad: function (options) {
+    // 获取后缀
+    if (typeof options.scene !== "undefined") this.setData({ suffix: options.scene })
+    // 配置查询条件
+    if (typeof options.year !== "undefined" && options.year !== "不限") this.setData({ "query.item12": options.year })
+    if (typeof options.province !== "undefined" && options.province !== "不限") this.setData({ "query.item01": options.province })
+    this.search();
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: '中国人民银行招聘岗位匹配查询系统',
+      path: "/pages/position-2021-jryh-pboc/index?scene=" + this.data.suffix,
+      imageUrl: 'http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/jryh/share-20210118.jpg'
+    }
+  },
+})
