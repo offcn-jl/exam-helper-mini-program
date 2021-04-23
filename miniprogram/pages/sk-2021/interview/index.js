3b109733034822de12ef798fe75c65cc1dad952b
ShadowWalker <master@rebeta.cn>
2021-04-23 11:53:42 +0800

    feat: 省面小管家;

---
+++ miniprogram/pages/sk-2021/interview/index.js
@@ -0,0 +1,39 @@
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    suffix: "",
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 获取后缀
+    if (typeof options.scene !== "undefined") {
+      this.setData({
+        suffix: options.scene
+      })
+    }
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: '2021吉林省考面试小管家',
+      imageUrl: 'http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/sk/2021/interview/share.jpg'
+    }
+  },
+
+  /**
+   * 用户点击右上角分享 分享到朋友圈
+   */
+  onShareTimeline: function () {
+    return {
+      title: '2021吉林省考面试小管家'
+    }
+  }
+})
