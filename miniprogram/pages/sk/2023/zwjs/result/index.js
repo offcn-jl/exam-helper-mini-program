fde36f374251eb615d2e0c5a9c35c41c086bc415
ShadowWalker <master@rebeta.cn>
2022-12-30 14:56:20 +0800

    feat: 2023省考五大系统;

---
+++ miniprogram/pages/sk/2023/zwjs/result/index.js
@@ -0,0 +1,161 @@
+const app = getApp()
+
+Page({
+  data: {
+    title:"2023省考历年分数线查询", // 标题
+    imageUrl:"https://jl.offcn.com/zt/ty/images/sk/2023/zwjs/share.jpg", // 分享时显示的图片
+    superiorLink:"/pages/sk/2023/zwjs/index", // 上级链接
+
+    suffixStr: "",          // 后缀
+    score:[],            // 历年分数线
+    query: {             // 查询条件
+      zwcode: "jl",      // 地区（吉林省 jl）
+      page: 0,           // 页码
+
+      areaname: "",      // 地市
+      danwei_name: "",   // 部门名称
+      xitong: "",        // 部门性质
+      zhiwei_name: "",   // 职位名称
+
+      zwyear: "",        // 年份
+      xueli: "",         // 学历
+      zzmm: "",          // 政治面貌
+      zhuanye: "",       // 专业
+      sstimes: new Date().getTime()
+    }, 
+    zcounts: 0, //最大数据量
+    result: [], // 查询结果
+    showId: 0,
+  },
+
+  /**
+   * 监听页面滚动
+   * 用于 显示 header / 隐藏 header
+   */
+  onPageScroll: function (e) { this.selectComponent('#header').setData({ scrollTop: e.scrollTop }) },
+
+  // 上拉刷新
+  onReachBottom() {
+    if (this.data.result.length >= this.data.zcounts) {
+      wx.showToast({ title: '没有更多数据啦', icon: 'none' })
+      return
+    }
+    this.search()
+  },
+
+  //查询
+  search(){
+    let _this = this //作用域 
+    wx.showLoading({ title: '查询中...', mask: true })
+    wx.request({
+      url: "https://zw.offcn.com/wechat/zhiwei/search?sign=b4a8b2f7z4d3n6o0hs",
+      method: "post",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: _this.data.query,
+      success(res) {
+        wx.hideLoading() // 隐藏 loading
+        try {
+          let list = res.data
+          //数据导入data.result
+          _this.setData({
+            result: _this.data.result.concat(list.lists),//不替换原数据添加新数据
+            "query.page": parseInt(list.page)+1,//计算分页页数
+            zcounts: list.total   //总数量录入data
+          })
+        } catch (err) {//捕获错误并报错
+          getApp().methods.handleError({ err, title: "出错啦", content: '查询失败', reLaunch: true })
+        }
+      },
+      fail: err => {//获取失败后提示
+        wx.hideLoading() // 隐藏 loading
+        getApp().methods.handleError({ err: err, title: "出错啦", content: '查询失败', reLaunch: true })
+      }
+    })
+  },
+
+  onLoad: function (options) {
+    // 动态设置当前页面的标题
+    wx.setNavigationBarTitle({
+      title: this.data.title
+    })
+    
+    // 获取后缀
+    getApp().methods.getSuffix(options).then(res => {
+        this.setData(res);
+    }).catch(err => {
+        wx.hideLoading(); // 隐藏 loading
+        getApp().methods.handleError({
+            err: err,
+            title: "出错啦",
+            content: '获取后缀失败',
+            reLaunch: true
+        });
+    });
+    // 配置查询条件
+    // 职位要求
+    if (options.city !== "undefined"){
+      this.setData({ "query.areaname": options.city })
+    }
+    if (options.bmxz !== "undefined" && options.bmxz){
+      this.setData({ "query.xitong": options.bmxz })
+    }
+    if (options.bmmc !== "undefined" && options.bmmc){
+      this.setData({ "query.danwei_name": options.bmmc })
+    } 
+    if (options.zwmc !== "undefined" && options.zwmc){
+      this.setData({ "query.zhiwei_name": options.zwmc })
+    } 
+    // 个人条件
+    if (options.year !== "undefined" && options.year){
+      this.setData({ "query.zwyear": options.year })
+    }
+    if (options.xueli !== "undefined" && options.xueli){
+      this.setData({ "query.xueli": options.xueli })
+    } 
+    if (options.zzmm !== "undefined" && options.zzmm){
+      this.setData({ "query.zzmm": options.zzmm })
+    } 
+    if (options.zylb !== "undefined" && options.zylb){
+      this.setData({ "query.zhuanye": options.zylb })
+    } 
+    this.search();
+  },
+  // 可收放数据样式
+  lnfsx: function (event) {
+    this.setData({
+      showId: event.currentTarget.dataset.index
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: this.data.title,
+      path: this.data.superiorLink+"?" + this.data.suffixStr,
+      imageUrl: this.data.imageUrl
+    }
+  },
+})
---
+++ miniprogram/pages/sk/2023/zyfl/result/index.js
@@ -0,0 +1,127 @@
+const app = getApp()
+
+Page({
+  data: {
+    title:"2023吉林省公务员考试专业分类查询系统",// 标题  
+    imageUrl:"https://jl.offcn.com/zt/ty/images/sk/2023/zyfl/share.jpg",// 分享时显示的图片
+    superiorLink:"/pages/sk/2023/zyfl/index", //上级链接
+
+    actid:"41667", // zg99id
+
+    suffixStr: "", // 后缀
+    showId: 0, // 当前显示的元素的 ID
+    query: { // 查询条件
+      limits: 10,
+      order:0, //正序排列
+      sstime: new Date().valueOf(),
+    }, 
+    zcounts: 0,//最大数据量
+    result: [], // 查询结果
+  },
+
+  /**
+   * 监听页面滚动
+   * 用于 显示 header / 隐藏 header
+   */
+  onPageScroll: function (e) { this.selectComponent('#header').setData({ scrollTop: e.scrollTop }) },
+
+  //可收放数据样式
+  more: function (event) {
+    this.setData({
+      showId: event.currentTarget.dataset.index
+    })
+  },
+
+  // 上拉刷新
+  onReachBottom() {
+    if (this.data.result.length >= this.data.zcounts) {
+      wx.showToast({ title: '没有更多数据啦', icon: 'none' })
+      return
+    }
+    this.search()
+  },
+
+  //查询
+  search(){
+    let _this = this //作用域 
+    wx.showLoading({ title: '查询中...', mask: true })
+    wx.request({
+      url: "https://zg99.offcn.com/index/chaxun/getfylist?actid="+_this.data.actid+"&callback=?",
+      data: _this.data.query,
+      success(res) {
+        wx.hideLoading() // 隐藏 loading
+        try {
+          let list = JSON.parse(res.data.substring(1, res.data.length - 1))//去头尾（）,转为json对象
+          console.log("list:",list)
+          if (list.status !== 1) {//如果status不等于1，弹出错误提示
+            wx.showToast({ title: list.msg, icon: 'none' })
+            return  
+          }
+          if (list.lists.length <= 0) {//如果内容长度小于等于0，弹出无数据提示
+            wx.showToast({ title: '没有更多数据啦', icon: 'none' })
+            return
+          }
+          //数据导入data.result
+          _this.setData({
+            result: _this.data.result.concat(list.lists),//不替换原数据添加新数据
+            "query.page": (_this.data.result.concat(list.lists).length / _this.data.query.limits)+1,//计算分页页数
+            zcounts: list.zcounts   //总数量录入data
+          })
+        } catch (err) {//捕获错误并报错
+          getApp().methods.handleError({ err, title: "出错啦", content: '查询失败', reLaunch: true })
+        }
+      },
+      fail: err => {//获取失败后提示
+        wx.hideLoading() // 隐藏 loading
+        getApp().methods.handleError({ err: err, title: "出错啦", content: '查询失败', reLaunch: true })
+      }
+    })
+  },
+
+  onLoad: function (options) {
+    // 动态设置当前页面的标题
+    wx.setNavigationBarTitle({
+      title: this.data.title
+    })
+
+    // 获取后缀信息并保存
+    getApp().methods.getSuffix(options).then(res=>{
+        this.setData(res); // 保存后缀信息
+    })
+
+    // 配置查询条件
+    if (typeof options.zylb !== "undefined" && options.zylb !== "不限") this.setData({ "query.major2": options.zylb })
+    this.search();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: this.data.title,
+      path: this.data.superiorLink+"?" + this.data.suffixStr,
+      imageUrl: this.data.imageUrl
+    }
+  },
+})
