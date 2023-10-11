2572f20c3e02507c556aa95bbf942b3c8536e836
lp <3422924002@qq.com>
2023-10-11 15:05:59 +0800

    feat:国考招考人数预测

---
+++ miniprogram/pages/sydw/yuce/index.js
@@ -0,0 +1,238 @@
+// pages/sydw/2022/yb/shaif/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        title:"2024国考活动",
+        imageUrl: "https://jl.offcn.com/zt/ty/2023images/exam-helper-mini/2024gkycrs.jpg", // 背景图片
+        successimageUrl:"https://jl.offcn.com/zt/ty/2023images/exam-helper-mini/success-gk.jpg",
+        shareImages: "https://jl.offcn.com/zt/ty/2023images/exam-helper-mini/2023thmhlnfs-share.jpg", // 分享时显示的图片
+        CRMEFSID: "2b057f22a081112e3c931e753ed1f14f ", // CRM 活动表单 ID
+        actid: "54022", //zg99id
+        gradeValue: 0, // 成绩
+
+        suffix: {}, // 后缀
+        suffixStr: "", // 后缀 字符串
+
+        phone: "", // 用户手机号码
+        openid: "", // openid
+        signed: false
+    },
+
+
+
+    // 监听筛选条件（获取笔试成绩录入表里）
+    getInputValue(e) {
+        var _this = this
+        var regu = /^[0-9]+\.?[0-9]*$/;
+        if (e.detail.value == "") {
+            _this.setData({
+                active: false
+            })
+        } else if (!regu.test(e.detail.value)) {
+            wx.showToast({
+                title: '请输入数字',
+                icon: 'error'
+            })
+            _this.setData({
+                active: true
+            })
+        } else {
+            _this.setData({
+                gradeValue: Number(e.detail.value),
+                active: true
+            })
+        }
+    },
+
+    // 手动检查 SSO 登录状态
+    SSOCheckManual: function () {
+        getApp().methods.SSOCheckManual({
+            crmEventFormSID: this.data.CRMEFSID,
+            suffix: {
+                suffix: this.data.suffix,
+                suffixStr: this.data.suffixStr
+            },
+            remark: this.data.CRMRemark,
+            callback: ({
+                phone,
+                openid
+            }) => {
+                this.setData({
+                    phone,
+                    openid
+                });
+                this.search()
+            }
+        });
+    },
+
+    //查询
+    search() {
+        let _this = this //作用域 
+
+        if (!_this.data.gradeValue) {
+            wx.showToast({
+                title: '请输入数字',
+                icon: 'error'
+            })
+            return
+        }
+       
+        wx.request({
+            url: "https://zg99.offcn.com/index/biaodan/writelogs",
+            data: {
+                actid: this.data.actid,
+                isagree: true,
+                phone: getApp().globalData.user.username,
+
+                fenshu: this.data.gradeValue,
+                sstime: new Date().valueOf()
+
+            },
+            success(res) {
+                //console.log(registerRes);
+                try {
+                    let registerRes = JSON.parse(res.data.substring(1, res.data.length - 1)) //去头尾（）,转为json对象
+                    if (registerRes.status !== 1) { //如果status不等于1，弹出错误提示
+                        if(registerRes.msg==="记录失败"){
+                            wx.showToast({
+                                title: "预测人数已提交",
+                            })
+                            _this.setData({signed: true });
+                            return
+                        }
+                        wx.showToast({
+                            title: registerRes.msg,
+                            icon: 'none'
+                        })
+                        return
+                    }
+                    _this.setData({signed: true });
+                    wx.showToast({
+                        title: "提交成功",
+                    })
+                } catch (err) { //捕获错误并报错
+                    getApp().methods.handleError({
+                        err,
+                        title: "出错啦",
+                        content: '提交失败',
+                        reLaunch: true
+                    })
+                }
+            },
+            fail: err => { //获取失败后提示
+                wx.hideLoading() // 隐藏 loading
+                getApp().methods.handleError({
+                    err: err,
+                    title: "出错啦",
+                    content: '提交失败',
+                    reLaunch: true
+                })
+            }
+        })
+     
+    },
+
+    showmore: function (event) {
+        this.setData({
+            showIndex: event.currentTarget.dataset.index
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+        if (!options.actid) {
+            getApp().methods.handleError({
+                err: null,
+                title: "出错啦",
+                content: '缺少活动id',
+                reLaunch: true
+            });
+            return
+        }
+        const _this = this;
+        wx.showLoading({
+            title: '加载中'
+        });
+
+
+        
+            this.setData({
+                actid: options.actid,
+                CRMEFSID: this.data.CRMEFSID,
+                imageUrl: this.data.imageUrl,
+                shareImages: this.data.shareImages,
+    
+            })
+        
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+        // if (this.data.yearValue !== "" && this.data.bankValue !== "" && this.data.provenceValue !== "" && this.data.cityValue !== "" && this.data.graduatesValue !== "" && this.data.cetValue !== "" && this.data.result.length < this.data.count) {
+        //     this.search();
+        // }
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+        return {
+            title: this.data.title,
+            shareImages: this.data.shareImages,
+        }
+    },
+
+    /**
+     * 用户点击右上角分享 分享到朋友圈
+     */
+    onShareTimeline: function () {
+        return {
+            title: this.data.title,
+        }
+    }
+})
